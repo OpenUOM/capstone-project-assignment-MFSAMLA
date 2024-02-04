9a2d8c832be3817060af16acae1968aaf42a052e
MFSAMLA <s92065136@ousl.lk>
2024-02-04 12:40:26 +0530

    search method updated

--- frontend/src/app/components/student-table/student-table.component.ts
+++ frontend/src/app/components/student-table/student-table.component.ts
@@ -51,13 +51,13 @@ export class StudentTableComponent implements OnInit {
     })
   }
 
-  search(value) {
+  search(getStudentData) {
     let foundItems = [];
-    if (value.length <= 0) {
+    if (getStudentData.length <= 0) {
       this.getStudentData();
     } else {
       let b = this.studentData.filter((student) => {
-        if (student[0].name.toLowerCase().indexOf(value) > -1) {
+        if (student[0].name.toLowerCase().indexOf(getStudentData) > -1) {
           foundItems.push(student)
         }
       });
--- frontend/src/app/components/teacher-table/teacher-table.component.ts
+++ frontend/src/app/components/teacher-table/teacher-table.component.ts
@@ -60,13 +60,13 @@ export class TeacherTableComponent implements OnInit {
     })
   }
 
-  search(value) {
+  search(getTeacherData) {
     let foundItems = [];
-    if (value.length <= 0) {
+    if (getTeacherData.length <= 0) {
       this.getTeacherData();
     } else {
       let b = this.teacherData.filter((teacher) => {
-        if (teacher[0].name.toLowerCase().indexOf(value) > -1) {
+        if (teacher[0].name.toLowerCase().indexOf(getTeacherData) > -1) {
           foundItems.push(teacher)
         }
       });
