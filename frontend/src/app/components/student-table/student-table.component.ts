2e7f257f6adb318b90c616e9d763dc104baff894
MFSAMLA <s92065136@ousl.lk>
2024-02-04 12:48:09 +0530

    changes in search method

--- frontend/src/app/components/student-table/student-table.component.ts
+++ frontend/src/app/components/student-table/student-table.component.ts
@@ -51,13 +51,13 @@ export class StudentTableComponent implements OnInit {
     })
   }
 
-  search(getStudentData) {
+  search(value) {
     let foundItems = [];
-    if (getStudentData.length <= 0) {
+    if (value.length <= 0) {
       this.getStudentData();
     } else {
       let b = this.studentData.filter((student) => {
-        if (student[0].name.toLowerCase().indexOf(getStudentData) > -1) {
+        if (student[0].name.toLowerCase().indexOf(value) > -1) {
           foundItems.push(student)
         }
       });
